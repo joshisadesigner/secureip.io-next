9e5de1045de7e5f03698c4be884617bbb826b4eb
Josh <josue.delacruz@americavoice.com>
2022-02-04 11:36:54 -0600

    Create Routes contant variables

---
+++ shared/index.ts
@@ -0,0 +1,9 @@
+export enum SiteRoutes {
+  CloudServices="/cloud-services",
+  Retail = "/retail",
+  Wholesale = "/wholesale",
+  LockStorage = "/products/lock-storage",
+  PushNotificationConsole = "/products/push-notification-console",
+  news = "/news",
+  article = "/news/article"
+}
