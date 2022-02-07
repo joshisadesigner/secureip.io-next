ba6d6a3ea1092c58ca018f0ec204f23f1eefee43
Josh <josue.delacruz@americavoice.com>
2022-02-06 20:18:15 -0600

    Adjust Navbar markup and styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,63 +3,67 @@ import Link from "next/link";
 export default function Navbar():JSX.Element {
   return (
     <header>
-      <nav className="navbar navbar-default navbar-fixed-top">
+      <nav className="navbar navbar-default navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <div className="navbar-header">
-            <button type="button"
-                    className="navbar-toggle collapsed"
-                    data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1"
-                    aria-expanded="false">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"/>
-              <span className="icon-bar"/>
-              <span className="icon-bar"/>
-            </button>
-            <a className="navbar-brand" href="#">
+          <Link href="/">
+            <a className="navbar-brand">
               <img className="team__ceo" src="./assets/images/logo-sip-h.svg" alt="Secure IP Logo" />
             </a>
-          </div>
-          {/*Collect the nav links, forms, and other content for toggling*/}
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                  aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"/>
+          </button>
+          <div className="collapse navbar-collapse justify-content-md-end" id="navbarSupportedContent">
+
             <ul className="nav navbar-nav">
-              <li className="active">
-                <Link href="/">
-                  <a>Home</a>
+              <li className="nav-item active">
+                <Link href="/coud-services">
+                  <a className="nav-link">Cloud Services</a>
                 </Link>
               </li>
-              <li>
-                <Link href="wholesale.html">
-                  <a>Wholesale</a>
+              <li className="nav-item">
+                <Link href="/ratail">
+                  <a className="nav-link">Retail</a>
                 </Link>
               </li>
-              <li>
-                <Link href="retail.html">
-                  <a>Retail</a>
+              <li className="nav-item">
+                <Link href="wholesale">
+                  <a className="nav-link">Wholesale</a>
                 </Link>
               </li>
-              <li>
-                <Link href="our-products.html">
-                  <a>Our Products</a>
+              <li className="nav-item">
+                <Link href="/our-products">
+                  <a className="nav-link">Our Products</a>
                 </Link>
-
               </li>
-              <li>
-                <Link href="connectwithus.html">
-                  <a>Connect With Us</a>
-                </Link>
-
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                   data-bs-toggle="dropdown" aria-expanded="false">
+                  Our Products
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <Link href="/products/lock-storage">
+                      <a className="dropdown-item">Lock Storage</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/products/push-notification-console">
+                      <a className="dropdown-item">Push Notification Console</a>
+                    </Link>
+                  </li>
+                </ul>
               </li>
-              <li>
-                <Link href="news.html">
-                  <a>News</a>
+              <li className="nav-item">
+                <Link href="/news">
+                  <a className="nav-link">News</a>
                 </Link>
               </li>
             </ul>
           </div>
-          {/*/.navbar-collapse*/}
         </div>
-        {/*/.container-fluid*/}
       </nav>
     </header>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "../styles/main.scss";
+import {useEffect} from "react";
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    // Run code on client-side only : ensure document is here
+    if (typeof document !== undefined) {
+      // load JS bootstrap dependency
+      require('bootstrap/dist/js/bootstrap')
+    }
+    // Run useEffect only once
+    // Read https://css-tricks.com/run-useeffect-only-once/
+  }, [])
+
   return <Component {...pageProps} />
 }
