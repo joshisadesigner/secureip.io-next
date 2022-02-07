8d6b94b2d22462a32a0fc7514a3fdc246bcef112
Josh <josue.delacruz@americavoice.com>
2022-02-06 22:16:59 -0600

    Add home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,78 @@
 export default function Home() {
   return (
     <>
+      <div className="masthead masthead-home">
+        <section className="container-fluid">
+          <div className="row justify-content-center">
+            <div className="col-xs-12">
+              <h1 className="text-center">
+                <span className="text-size-huge">Telecom</span>
+                <span className="text-size-huge text-brand-1">Technology</span>
+                <span className="text-size-huge ">Services</span>
+              </h1>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-xs-12 col-sm-8 col-md-6 col-md-push-3 col-sm-push-2">
+              <p>
+                Secure IP is a leading <strong>telecom technology services</strong> including prepaid pin less,
+                wholesale international long distance, mobile top-ups, and international e-gift cards. The company’s
+                commitment to offer the highest quality services with advanced engineering techniques, high-grade
+                network facilities, and leading edge technology, consistently draws top marks for Secure IP from its
+                industry peers and customers alike.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-xs-12 col-sm-8 col-sm-push-2">
+              <div className="row">
+                <div className="col-sm-12 text-center">
+                  <h2 className="">
+                    <span className="text-brand-1 text-size-big">INNOVATION</span>
+                  </h2>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-12 text-center">
+                  Since its start in 2006, Secure IP has been a company dedicated to providing high quality <strong>telecom
+                  services</strong>. Through its <a href="https://www.americavoice.com/">America Voice</a> brand, the
+                  company provides its services at over 5,000 retail locations throughout the US. Secure IP’s wholesale
+                  division provides premium international long distance to carriers globally.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-9">
+              <h3 className="m-t-none  m-b-lg">MEET THE TEAM</h3>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="gal">
+                <img src="https://secureip.io/app/uploads/2018/10/01.jpg" alt="01"/>
+                <img src="https://secureip.io/app/uploads/2018/10/team-02.jpg" alt="team-02"/>
+                <img src="https://secureip.io/app/uploads/2018/10/team-03.jpg" alt="team-03"/>
+                <img src="https://secureip.io/app/uploads/2018/10/team-04.jpg" alt="team-04"/>
+                <img src="https://secureip.io/app/uploads/2018/10/team-05.jpg" alt="team-05"/>
+                <img src="https://secureip.io/app/uploads/2018/10/team-06.jpg" alt="team-06"/>
+                <img src="https://secureip.io/app/uploads/2018/10/team-07.jpg" alt="team-07"/>
+                <img src="https://secureip.io/app/uploads/2018/10/team-08.jpg" alt="team-08"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
