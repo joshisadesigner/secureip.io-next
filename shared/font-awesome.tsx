64bb8fd6e375d07d87e3213eb07142e1d624ffd6
Josh <josue.delacruz@americavoice.com>
2022-02-06 20:25:56 -0600

    Create Font Awesome library component

---
+++ shared/font-awesome.tsx
@@ -0,0 +1,12 @@
+import { library, config} from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faFacebook,
+  faTwitter
+} from "@fortawesome/free-brands-svg-icons";
+
+config.autoAddCss = false;
+library.add(
+  faFacebook,
+  faTwitter
+);
