745c287e195314d4f48bcd35c6b0ae88ca670a61
Josh <josue.delacruz@americavoice.com>
2022-02-06 23:42:07 -0600

    Add Cloud Services page component

---
+++ pages/cloud-services.tsx
@@ -0,0 +1,50 @@
+export default function CloudServices():JSX.Element {
+  return (
+    <>
+      <div className="masthead"/>
+      <section className="p-notop">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <header>
+                <h2>CLOUD SERVICES</h2>
+              </header>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <p>
+                <strong>Cloud Services</strong> is a product of Secure IP, has been in the telecommunications market for over a decade. With the evolution of industry and technology, Secure IP has expanded its services and products; as a result of this evolution, we introduce:  <strong>Secure Ip Cloud Services</strong>, focused to deliver a pack of digital solutions on cloud with the latest technology, equipment and experienced staff, all this combined with affordable prices. Our products are: <a href="https://cloud.secureip.io/cart.php?gid=4">SSL Certificates</a>, <a href="https://cloud.secureip.io/cart.php?a=add&amp;domain=register">Domain Purchase/Transfer.</a>
+              </p>
+              <p>
+                <a href="https://backup.secureip.io/"><strong>Lock Backup</strong></a> saves your important files where you back up your data for SMS, call log, contacts, photos, videos and documents on your phone available for Android &amp; iOS. Data backup can be configured manually or automatically and conveniently stores all your data securely in the cloud. With <strong><a href="https://backup.secureip.io/">Lock Backup</a></strong> you can upload and manage copies of files such as videos, Photos, documents, texts and call logs, own personal calendar and music. Plus, you can download and recover your files from the cloud no matter the disaster without any complications. The free version includes 10GB space, no expiring! And plans to expand storage capacity based on your needs and budget.
+              </p>
+            </div>
+            <div className="col-md-6">
+              <div className="mb-4">
+                <img
+                  src="https://secureip.io/app/uploads/2019/10/Placeholder_Cloud_Services_Oct_2019.png"
+                  width="1284"
+                  height="673"
+                  sizes="(max-width: 1284px) 100vw, 1284px"
+                  alt=""
+                  className="so-widget-image"
+                />
+              </div>
+              <div>
+                <iframe
+                  width="465"
+                  height="280"
+                  src="https://www.youtube.com/embed/8uUlHSUK4xU"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
