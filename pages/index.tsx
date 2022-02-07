bb2a9b99251b40a9937d2b5d68dcd17e9a19043b
Josh <josue.delacruz@americavoice.com>
2022-02-06 19:37:39 -0600

    Insert Navbar and Footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import Head from "next/head";
 import Layout, { siteTitle } from '../components/layout';
+import Navbar from "../components/navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <Layout home>
+    <>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <div>Welcome to SECURE IP!</div>
-    </Layout>
+      <Layout home>
+        <Navbar />
+        <main />
+        <Footer />
+      </Layout>
+    </>
   )
 }
